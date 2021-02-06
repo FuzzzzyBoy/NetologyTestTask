12be22f7f281276c10d6ce56222cee5f1975cbe1
Kirill Shakhansky <k.shakhansky@corp.mail.ru>
2021-02-06 21:23:03 +0300

    add xcode project

---
+++ NetologyTestTask/NetologyTestTask/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NetologyTestTask
+//
+//  Created by k.shakhansky on 06.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
