e3b654e298be64fabebfc8e6db5cdea82186c7bb
Kirill Shakhansky <k.shakhansky@corp.mail.ru>
2021-02-06 21:34:10 +0300

    get post

--- NetologyTestTask/NetologyTestTask/ViewController.swift
+++ NetologyTestTask/NetologyTestTask/ViewController.swift
@@ -7,10 +7,25 @@
 
 import UIKit
 
+struct Post: Codable {
+    let title: String
+    let body: String
+    let id: Int
+    let userId: Int
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data,
+               let post = try? JSONDecoder().decode(Post.self, from: data) {
+                print(post)
+            }
+        }.resume()
     }
 
 }
