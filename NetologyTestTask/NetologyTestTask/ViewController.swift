58a0d18831ce827cdc492d47b9ad6e824c993b92
Kirill Shakhansky <k.shakhansky@corp.mail.ru>
2021-02-06 21:57:54 +0300

    show title and body of post in labels

--- NetologyTestTask/NetologyTestTask/ViewController.swift
+++ NetologyTestTask/NetologyTestTask/ViewController.swift
@@ -16,21 +16,56 @@ struct Post: Codable {
 
 class ViewController: UIViewController {
 
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = .zero
+        return label
+    }()
+
+    private lazy var bodyLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = .zero
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!
+
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: stackView.topAnchor),
+            view.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+            view.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: stackView.trailingAnchor)
+        ])
+
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(bodyLabel)
+
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/2")!
         let documentDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let fileURL = documentDirectoryURL.appendingPathComponent("post.json")
         if let data = try? Data(contentsOf: fileURL) {
             let post = try! JSONDecoder().decode(Post.self, from: data)
-            print(post)
+            titleLabel.text = post.title
+            bodyLabel.text = post.body
         } else {
-            URLSession.shared.dataTask(with: url) { (data, response, error) in
+            URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
                 let post = try! JSONDecoder().decode(Post.self, from: data!)
                 let postData = try! JSONEncoder().encode(post)
                 try! postData.write(to: fileURL)
-                print("Save to disk")
+                DispatchQueue.main.async {
+                    self?.titleLabel.text = post.title
+                    self?.bodyLabel.text = post.body
+                }
             }.resume()
         }
     }
