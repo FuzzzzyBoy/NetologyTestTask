3e2e11f5ebe3e32b4e147bd3420a637a2b0256ae
Kirill Shakhansky <k.shakhansky@corp.mail.ru>
2021-02-07 12:18:13 +0300

    3th iteration

--- NetologyTestTask/NetologyTestTask/SceneDelegate.swift
+++ NetologyTestTask/NetologyTestTask/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController02()
+        window.rootViewController = ViewController03()
         window.makeKeyAndVisible()
         self.window = window
     }
